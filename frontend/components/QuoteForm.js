ece837377513bf5ebacd0f48f885d0b0305788c5
Farhana Aktar <farhanaaktar@live.co.uk>
2024-08-23 20:29:59 +0100

    do it not fixed

--- frontend/components/QuoteForm.js
+++ frontend/components/QuoteForm.js
@@ -5,19 +5,41 @@ const CHANGE_INPUT = 'CHANGE_INPUT'
 const RESET_FORM = 'RESET_FORM'
 
 // 👇 create your initial state object here
+const initialState = {
 
+  authorName : '',
+  quoteText: ''
+}
 // 👇 create your reducer function here
+const reducer = (state, action) => {
+  switch(action.type) {
+    case CHANGE_INPUT: {
+      const {name, value} = action.payload
+      return {...state, [name]: value}
+    } 
+    case RESET_FORM: {
+      return {authorName: '', quoteText: ''}
+    }
+    default: {
+      return state
+    }
+  }
+}
 
-export default function TodoForm({ createQuote = () => { } }) {
+export default function TodoForm({ createQuote  }) {
   // 👇 use the reducer hook to spin up state and dispatch
-
-  const onChange = () => {
-    // 👇 implement
+const [state, dispatch] = useReducer(reducer, initialState)
+  const onChange = ({target: {name, value}}) => {
+    dispatch({type: CHANGE_INPUT, payload: {name, value}})
   }
   const resetForm = () => {
+    dispatch({type: RESET_FORM})
     // 👇 implement
   }
-  const onNewQuote = () => {
+  const onNewQuote = (evt) => {
+    evt.preventDefualt()
+    const {authorName, quoteText} = state
+    createQuote({authorName, quoteText})
     // 👇 implement
     resetForm()
   }
@@ -28,6 +50,7 @@ export default function TodoForm({ createQuote = () => { } }) {
       <h3>New Quote Form</h3>
       <label><span>Author:</span>
         <input
+        value={state.authorName}
           type='text'
           name='authorName'
           placeholder='type author name'
